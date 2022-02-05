98f1d200d6752af0e0abf15fcc4623acdaaee269
Pavel <pavel@liram.com>
2022-02-05 17:52:33 +0200

    Cleanup; added db cleanup after tests runnung

--- src/test/java/testUtils/BeforeAllTests.java
+++ src/test/java/testUtils/BeforeAllTests.java
@@ -1,10 +1,11 @@
 package testUtils;
 
+import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import utils.AppSettings;
 
-public class BeforeAllTests implements BeforeAllCallback {
+public class BeforeAllTests implements BeforeAllCallback, AfterAllCallback {
     @Override
     public void beforeAll(ExtensionContext extensionContext) throws Exception {
         System.out.println("Run tests started - database cleanup...");
@@ -20,5 +21,10 @@ public class BeforeAllTests implements BeforeAllCallback {
 
     private void cleanupDatabase() {
         new DbCleanupHelper().cleanupDatabase();
+    }
+
+    @Override
+    public void afterAll(ExtensionContext extensionContext) throws Exception {
+        cleanupDatabase();
     }
 }
